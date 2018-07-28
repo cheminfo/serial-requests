5f5ed0fae158ca80168732e86215a2e1919ba9cc
Luc Patiny <luc@patiny.com>
2018-07-28 12:02:55 +0200

    update to last version of serialport

--- example/DeviceManager.js
+++ example/DeviceManager.js
@@ -3,28 +3,30 @@
 const DeviceManager = require('..').DeviceManager;
 
 var d = new DeviceManager({
-    optionCreator: function (portInfo) {
-        if (portInfo.manufacturer === 'Keyspan') {
-            return {
-                baudrate: 9600,
-                getIdCommand: '!SHOW HOST_NAME\n',
-                getIdResponseParser: function (buffer) {
-                    var m = /^Host Name = (.*)\r\n$/.exec(buffer);
-                    if (m && m[1]) {
-                        return m[1];
-                    }
-                    throw new Error('Could not parse id response')
-                },
-                checkResponse: function (buffer) {
-                    return buffer.endsWith('\n');
-                }
-            }
+  optionCreator: function(portInfo) {
+    if (portInfo.manufacturer === 'Keyspan') {
+      return {
+        baudRate: 9600,
+        getIdCommand: '!SHOW HOST_NAME\n',
+        getIdResponseParser: function(buffer) {
+          var m = /^Host Name = (.*)\r\n$/.exec(buffer);
+          if (m && m[1]) {
+            return m[1];
+          }
+          throw new Error('Could not parse id response');
+        },
+        checkResponse: function(buffer) {
+          return buffer.endsWith('\n');
         }
+      };
     }
+  }
 });
 
-d.addRequest('blaster_test_epfl', '!SHOW HOST_NAME\n').then(res => {
+d.addRequest('blaster_test_epfl', '!SHOW HOST_NAME\n')
+  .then(res => {
     console.log(res);
-}).catch(err => {
+  })
+  .catch(err => {
     console.log('add request failed', err);
-});
+  });
